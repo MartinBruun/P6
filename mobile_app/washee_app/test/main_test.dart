a5453e6965f904b595bd8444c7319b5082f07ff9
Rallefar1103 <rasmus@turftank.com>
2022-02-14 14:39:04 +0100

    adds initial test

---
+++ mobile_app/washee_app/test/main_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'should setup initial test',
+    () async {
+      // arrange
+
+      // act
+
+      // assert
+      assert(true);
+    },
+  );
+}
