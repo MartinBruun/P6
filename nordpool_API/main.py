b32f54f0b106fc6da264be552d260ca603d5917a
mkju <mkju19@student.aau.dk>
2022-02-21 00:11:49 +0100

    can now fetch  some data
    still need to clean up the data

--- nordpool_API/main.py
+++ nordpool_API/main.py
@@ -28,7 +28,7 @@ class NordpoolAPI:
         file = open("data.csv", 'w')
 
         for line in data:
-            file.write(line + "\n")
+            file.write(str(line) + "\n")
 
         file.close()
 
@@ -36,6 +36,16 @@ class NordpoolAPI:
         for line in data:
             print(line)
         print("-----END-----")
+
+    def __decode(self, data):
+
+        decoded_data = []
+
+        for line in data:
+            for chunk in line.split(b'\r\n'):
+                (str(chunk))
+                decoded_data.append(chunk)
+        return decoded_data
 
     def ftp_retrieve(self, path, filename):
         ftp = ftplib.FTP(self.__url)
@@ -45,16 +55,41 @@ class NordpoolAPI:
 
         data = []
 
-        ftp.retrlines("RETR " + filename, data.append)
+        # # The normal way to retrieve data with ftp. this doesn't work with the data we need for some reason
+        # ftp.retrlines("RETR " + filename, data.append)
+
+        ftp.retrbinary("RETR " + filename, data.append)
+
+        ftp.quit()
+
+        data = self.__decode(data)
 
         self.__save_data(data)
 
+    def ftp_dir(self, path):
+        ftp = ftplib.FTP(self.__url)
+        ftp.login(self.__username, self.__password)
+
+        ftp.cwd(path)
+
+        data = []
+
+        ftp.dir(data.append)
+
         ftp.quit()
+
+        self.__print_data(data)
 
 
 if __name__ == '__main__':
     nordpool = NordpoolAPI()
-    path = "/Operating_data/Denmark"
-    file = "podk2207.sdv"
-    nordpool.ftp_retrieve(path, file)
 
+    # # path and file that works with regular decoding
+    # path = "/Operating_data/Denmark"
+    # file = "podk2207.sdv"
+
+    path = "/Elspot/Elspot_prices/Denmark/Denmark_West"
+    file = "odedkk22.sdv"
+
+    nordpool.ftp_retrieve(path, file)
+    # nordpool.ftp_dir(path)
