3d035ed1ec90a3f7db223283a95fceb9a00f091d
MartinBruun <mbm93fen@gmail.com>
2022-02-21 10:07:06 +0100

    Remove prev password

--- washee_web/app/washee_web/urls.py
+++ washee_web/app/washee_web/urls.py
@@ -36,5 +36,5 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
-    path('', include(router.urls)),
+    path('', include(router.urls))
 ]
