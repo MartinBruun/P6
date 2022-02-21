f06058c2baf2221c877359825c5d2f738263189a
MartinBruun <mbm93fen@gmail.com>
2022-02-21 13:18:16 +0100

    Init commit

--- washee_web/app/account/apps.py
+++ washee_web/app/account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ServicesConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'services'
+    name = 'account'
