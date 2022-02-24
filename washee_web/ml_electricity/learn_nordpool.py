a5a402b19f098ff7e862000c0a9bf7b0925e935d
mkju <mkju19@student.aau.dk>
2022-02-25 00:03:46 +0100

    pipeline prototype

--- washee_web/ml_electricity/learn_nordpool.py
+++ washee_web/ml_electricity/learn_nordpool.py
@@ -1,5 +1,31 @@
-from nordpool_API.Nordpool import nordpool
-import machine_learning
+import numpy
+from nordpool_API.Nordpool import NordpoolAPI
+from machine_learning.machine_learning import NordpoolML
 
-def learn():
-    nord = nordpool()
+def learn(path, file):
+    nordpool = NordpoolAPI()
+    nordpool.ftp_retrieve(path, file)
+    
+    data = get_data()
+    
+    ml = NordpoolML(data)
+    ml.train_linear_regression()
+    ml.predict([[1,2]])
+
+
+def get_data():
+
+    dataset = {
+        'day': ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+        'time': [1 ,2 ,3 ,4 ,5 ,6 , 7],
+        'price': [123, 234, 45, 456, 456, 567, 234]
+    }
+
+    return pandas.DataFrame(dataset)
+
+if __name__ == '__main__':
+    path = "/Elspot/Elspot_prices/Denmark/Denmark_West"
+    file = "odedkk22.sdv"
+    
+    learn(path, file)
+    
