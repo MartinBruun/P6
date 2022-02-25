ad6c305cfcc9b3416c6520b4d1068e1884e1b6d1
MartinBruun <mbm93fen@gmail.com>
2022-02-25 10:47:57 +0100

    Initial tests created

---
+++ washee_app/test/exception_handler_test.dart
@@ -0,0 +1,12 @@
+
+
+// Test that an Exception Handler Class can be initialized
+
+// Test that the Exception Handler can take an Exception, and then
+// either write to a log, print to the console OR crash the program
+// (This represents different security levels 
+//  / if we work in development or production)
+
+// Writes to a log should be timestamped and (if applicable) 
+//what user did it, and what action was the cause
+s
