e6c5196f169641ffbb70d13a30a854ee90187203
MartinBruun <mbm93fen@gmail.com>
2022-02-25 10:32:21 +0100

    Initial tests created

---
+++ washee_box/white_list.py
@@ -0,0 +1,12 @@
+
+
+class WhiteList:
+    
+    def __init__(self):
+        self.subjects = []
+        self.objects = []
+        self.actions = []
+        
+        
+    def _access_control_matrix(self):
+        pass
