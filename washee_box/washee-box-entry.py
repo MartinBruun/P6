c256373abee412017d258d660285be2af48b0e63
saxjax <saxjax@saxjax.dk>
2022-02-27 01:57:47 +0100

    added some endpoints

--- washee_box/washee-box-entry.py
+++ washee_box/washee-box-entry.py
@@ -8,15 +8,39 @@ machine_name = ['#1','#2','#3','#4']
 def menu():
     machine_name_string = ""
     for machineName in machine_name:
-        machine_name_string = machine_name_string + "\n  <a href='/light'>" + machineName +"</a>"
+        machine_name_string = machine_name_string + "\n  <a href='/unlock'>" + machineName +"</a>"
 
-    return "<p> booking kalender</p> \n <p>oplås maskine:" + machine_name_string + "</p>"
+    return "<p><a href='/booking'> booking kalender</a></p> \n <p>oplås maskine:" + machine_name_string + "</p>"+ "\n <p><a href='/settings'>update settings</a></p>"+"\n<p><a href='/accepted_users'>update users</a></p> "
 
-@app.route('/light')
-def light():
+@app.route('/unlock')
+def unlock():
     file = open(r'./use_cases/relay.py', 'r').read()
     exec(file)
     return "<a href='/'> Machine has been unlocked </a>"
+
+@app.route('/lock')
+def lock():
+    file = open(r'./use_cases/relay.py', 'r').read()
+    exec(file)
+    return "<a href='/'> Machine has been locked </a>"
+
+@app.route('/settings')
+def update_settings():
+    file = open(r'./use_cases/relay.py', 'r').read()
+    exec(file)
+    return "<a href='/'> Settings has been updated </a>"
+
+@app.route('/accepted_users')
+def update_accepted_users():
+    file = open(r'./use_cases/relay.py', 'r').read()
+    exec(file)
+    return "<a href='/'> Accepted users has been updated </a>"
+
+@app.route('/booking')
+def display_booking():
+    file = open(r'./use_cases/relay.py', 'r').read()
+    exec(file)
+    return '<div class="Calendar"><a href="/"> Mandag | | </a><a href="/"> Tirsdag | | </a><a href="/"> Onsdag | | </a><a href="/"> Torsdag | | </a><a href="/"> Fredag | | </a><a href="/"> Lørdag | | </a><a href="/"> Søndag | | </a></div>'
 
 if __name__ == "__main__":
     debug = False if int(os.environ.get("BOX_DEBUG",default="1")) == 0 else True
