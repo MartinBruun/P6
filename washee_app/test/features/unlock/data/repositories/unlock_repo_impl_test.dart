d64900283e9d4f0c2cfe852c471634eaa6f7f252
Rallefar1103 <rasmus@turftank.com>
2022-03-07 09:23:03 +0100

    prepares tests for unlocking

---
+++ washee_app/test/features/unlock/data/repositories/unlock_repo_impl_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'should return false when there is not internet connection',
+    () async {
+      // arrange
+
+      // act
+
+      // assert
+    },
+  );
+
+  test(
+    'should return true when a successfull unlocking is done ',
+    () async {
+      // arrange
+
+      // act
+
+      // assert
+    },
+  );
+
+  test(
+    'should return false when unlocking fails',
+    () async {
+      // arrange
+
+      // act
+
+      // assert
+    },
+  );
+}
