ee22a048e027a54d38716c2bfed5f6e39bcb6b1a
Rallefar1103 <rasmus@turftank.com>
2022-03-07 09:58:03 +0100

    generates mock for repository and sets up the fromJson on the WasheeBox model

--- washee_app/lib/core/washee_box/washee_entity.dart
+++ washee_app/lib/core/washee_box/washee_entity.dart
@@ -3,9 +3,17 @@ import 'package:equatable/equatable.dart';
 class WasheeBox extends Equatable {
   final String ssid;
   final String name;
+  final String machineType;
+  final DateTime startTime;
+  final DateTime endTime;
 
-  WasheeBox({required this.ssid, required this.name});
+  WasheeBox(
+      {required this.ssid,
+      required this.name,
+      required this.machineType,
+      required this.startTime,
+      required this.endTime});
 
   @override
-  List<Object?> get props => [ssid, name];
+  List<Object?> get props => [ssid, name, machineType, startTime, endTime];
 }
--- washee_app/test/features/unlock/data/repositories/unlock_repo_impl_test.dart
+++ washee_app/test/features/unlock/data/repositories/unlock_repo_impl_test.dart
@@ -1,36 +1,81 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:washee/core/network/network_info.dart';
+import 'package:washee/features/unlock/data/datasources/unlock_remote.dart';
+import 'package:washee/features/unlock/data/repositories/unlock_repo_impl.dart';
 
+import 'unlock_repo_impl_test.mocks.dart';
+
+@GenerateMocks([], customMocks: [
+  MockSpec<NetworkInfo>(as: #MockNetworkInfo, returnNullOnMissingStub: true),
+  MockSpec<UnlockRemote>(as: #MockRemote, returnNullOnMissingStub: true)
+])
 void main() {
+  var mockRemote = MockRemote();
+  var mockNetworkInfo = MockNetworkInfo();
+  var repository =
+      UnlockRepositoryImpl(remote: mockRemote, networkInfo: mockNetworkInfo);
+
+  group("Internet connection", () {
+    test(
+      'should return true when there is a internet connection',
+      () async {
+        // arrange
+        when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+        // act
+        final result = await mockNetworkInfo.isConnected;
+
+        // assert
+        verify(mockNetworkInfo.isConnected);
+      },
+    );
+
+    test(
+      'should return false when there is no internet connection',
+      () async {
+        // arrange
+        when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+
+        // act
+        final result = await mockNetworkInfo.isConnected;
+
+        // assert
+        verify(mockNetworkInfo.isConnected);
+      },
+    );
+  });
+
   test(
-    'should return false when there is not internet connection',
+    'should return true when a successfull unlocking is done and there is a network conenction ',
     () async {
       // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      when(mockRemote.unlock()).thenAnswer((_) async => true);
 
       // act
+      final result = await repository.unlock();
 
       // assert
+      verify(mockRemote.unlock());
+      expect(result, true);
     },
   );
 
   test(
-    'should return true when a successfull unlocking is done ',
+    'should return false when unlocking fails because there is no active network connection',
     () async {
       // arrange
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
 
       // act
+      final result = await repository.unlock();
 
       // assert
-    },
-  );
-
-  test(
-    'should return false when unlocking fails',
-    () async {
-      // arrange
-
-      // act
-
-      // assert
+      verify(mockRemote.unlock()).called(0);
+      expect(result, false);
     },
   );
 }
