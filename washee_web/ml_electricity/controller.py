363de9be0d4b3b68413065e1063d40cdbc274c61
mkju <mkju19@student.aau.dk>
2022-03-16 23:43:33 +0100

    fixed controller

--- washee_web/ml_electricity/controller.py
+++ washee_web/ml_electricity/controller.py
@@ -7,7 +7,7 @@ class MIcontroller:
 
     def __init__(self):
         self.ml = NordpoolML()
-        self.preproces = Preprocessor()
+        self.preprocessor = Preprocessor()
 
     def update_data(self):
         path = "/Elspot/Elspot_prices/Denmark/Denmark_West"
@@ -17,7 +17,7 @@ class MIcontroller:
         nordpool.ftp_retrieve(path, file)
 
     def train(self):
-        data = self.preproces.get_data()
+        data = self.preprocessor.get_data()
         self.ml.split(data)
         self.ml.train()
 
@@ -29,17 +29,9 @@ class MIcontroller:
     #     return self.model.score()
 
 if __name__ == '__main__':
-
-    dataset = {
-            'day': [1, 2, 3, 3, 5, 6, 7],
-            'time': [1 ,2 ,3 ,4 ,5 ,6 , 7],
-            'price': [123, 234, 45, 456, 456, 567, 234]
-        }
-    data = pandas.DataFrame(dataset)
-
     controller = MIcontroller()
 
     controller.train()
-    print(controller.predict([[1,2], [2,4]]))
+    print(controller.predict([[1], [2]]))
     print(controller.ml.score())
     
