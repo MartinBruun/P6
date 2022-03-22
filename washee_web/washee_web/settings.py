7257964f8e8fef67ed2a7730f552bf0d6c66a5dc
MartinBruun <mbm93fen@gmail.com>
2022-03-22 19:01:07 +0100

    Add static and media files

--- washee_web/washee_web/settings.py
+++ washee_web/washee_web/settings.py
@@ -40,6 +40,7 @@ CORE_APPS = [
 ]
 
 OWN_APPS = [
+    "upload",
     "account",
     "booking",
     "location"
@@ -143,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
