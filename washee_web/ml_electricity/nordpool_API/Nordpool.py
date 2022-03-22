98b13489db94aae84c695228f3570645ca46fb2c
mkju19 <mkju19@student.aau.dk>
2022-03-22 17:05:37 +0100

    Nordpool api (#68)
    * get access to nordpool data
    
    first attempt at getting access to nordpools data
    
    * Data can now be retrieved
    
    * can now fetch  some data
    
    still need to clean up the data
    
    * added csv
    
    * moved files to ml_electricity
    
    * a
    
    * removed pip file
    
    * csv cleanup
    
    * decode of nordpool data
    
    * pipeline prototype
    
    * edited requirements
    
    * added a path for the csv file
    
    * Delete data.csv
    
    * Delete data.csv
    
    * imported pandas
    
    * Update .gitignore
    
    * added tests
    
    * added tests
    
    added tests for machine learning, preprocessor, machine prediction and nordpool_API
    
    * made it possible to execute through controller
    
    * Make docker work with pandas (#80)
    
    * fixed ports on box, and some docker stuff (#69)
    
    * Initial tests created
    
    * Remove stupid stupid error
    
    * Setting up unlock (#79)
    
    * adds a communicator for handling http request for the box
    
    * adds code for unlocking
    
    * adds singletons and dependency injection for unlocking feature
    
    * sets up the flow for unlocking the laundry machine in the UI
    
    * adds connectivity check
    
    Co-authored-by: Rallefar1103 <rasmus@turftank.com>
    
    * Initial test of new image
    
    * Update dockerfile
    
    Co-authored-by: Jakob Skov Søndergård <saxjax@saxjax.dk>
    Co-authored-by: Rallefar1103 <rasmus@turftank.com>
    
    * added some tests
    
    * added tests for machine_learning
    
    * restructure to make mocking easier
    
    * mock login info
    
    * set up initial test for nordpool.py
    
    * added preproccessing
    
    can now get nordpool data in a pandas dataframe
    
    * fixed index in preprocessor
    
    * mocked FTP successfully
    
    * Update nordpool_tests.py
    
    * added tests for ML_electricity
    
    * added method to reshape data to be used for MI
    
    * fixed controller
    
    * corrected MI to work with current data
    
    * added test for reshape
    
    * removed unnecessary  print
    
    * updated update_data
    
    * changed test to not use csv files
    
    * Update Nordpool.py
    
    * Delete test_nordpool.json
    
    * Update preprocessor.py
    
    * Update nordpool_tests.py
    
    Co-authored-by: MartinBruun <58165955+MartinBruun@users.noreply.github.com>
    Co-authored-by: Jakob Skov Søndergård <saxjax@saxjax.dk>
    Co-authored-by: Rallefar1103 <rasmus@turftank.com>

--- washee_web/ml_electricity/nordpool_API/Nordpool.py
+++ washee_web/ml_electricity/nordpool_API/Nordpool.py
@@ -1,34 +1,50 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from hashlib import new
 import json
 import ftplib
+import os
+from typing import Type
+from unittest import mock
+import csv
 
 
 class NordpoolAPI:
-    __url = ""
-    __username = ""
-    __password = ""
-    __access_data_path = "nordpool.json"
 
-    def __init__(self):
-        data = self.__get_nordpool_access_data()
+    __REQUIRED_COLUMNS = 35
+
+    def __init__(self, ftp = None):
+        
+        path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), "nordpool.json"))
+        
+        data = self.__get_nordpool_access_data(path)
 
         self.__url = data["url"]
         self.__username = data["username"]
         self.__password = data["password"]
 
-    def __get_nordpool_access_data(self):
-        file = open(self.__access_data_path)
+        self.__save_data_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), "..", "data.csv"))
+        
+        if ftp == None:
+            self.__ftp = ftplib.FTP(self.__url)
+        else:
+            self.__ftp = ftp
+
+    def __get_nordpool_access_data(self, path):
+        file = open(path)
 
         data = json.load(file)
-
+        
         return data
 
     def __save_data(self, data):
-        file = open("data.csv", 'w')
+        file = open(self.__save_data_path, 'w', newline="")
+        print(file)
+        print(type(file))
+        writer = csv.writer(file)
 
         for line in data:
-            file.write(str(line) + "\n")
+            writer.writerow(line.split(";"))
 
         file.close()
 
@@ -38,51 +54,83 @@ class NordpoolAPI:
         print("-----END-----")
 
     def __decode(self, data):
+        
+        # make all bytes into strings
+        for i in range(0, len(data)):
+            data[i] = str(data[i])
 
-        decoded_data = []
+        # split data chunks into lines and remove byte encoding (b'string here')
+        data_lines = []
+        for chunk in data:
+            for line in chunk.split('\\r\\n'):
+                line = (str(line)).lstrip("b'").rstrip("'").rstrip("\\r").lstrip("\\n")
+                data_lines.append(line)
 
-        for line in data:
-            for chunk in line.split(b'\r\n'):
-                (str(chunk))
-                decoded_data.append(chunk)
-        return decoded_data
+        # join lines that are split incorrecly
+        # lines are considered to be split incorrecly if they:
+        #   Are not in the header section (the first 5 lines)
+        #   && they have fewer than 35 semicolons
+        #   && that that end of file have not been reached
+        i = 0
+        for line in data_lines:
+            if (i > 5 
+                and self.__number_of_columns(line) < self.__REQUIRED_COLUMNS
+                and len(data_lines) > i + 1):
+
+                    data_lines[i+1] = str(line) + str(data_lines[i+1])
+                    data_lines.pop(i)
+            elif line == "":
+                data_lines.pop(i)
+
+            i += 1
+
+        return data_lines
+
+    def __number_of_columns(self, line):
+        count = 0
+        for i in line:
+            if i == ";":
+                count += 1
+        return count
 
     def ftp_retrieve(self, path, filename):
-        ftp = ftplib.FTP(self.__url)
-        ftp.login(self.__username, self.__password)
+        
+        self.__ftp.login(self.__username, self.__password)
 
-        ftp.cwd(path)
+        self.__ftp.cwd(path)
 
         data = []
 
         # # The normal way to retrieve data with ftp. this doesn't work with the data we need for some reason
         # ftp.retrlines("RETR " + filename, data.append)
 
-        ftp.retrbinary("RETR " + filename, data.append)
+        self.__ftp.retrbinary("RETR " + filename, data.append)
 
-        ftp.quit()
+        self.__ftp.quit()
 
         data = self.__decode(data)
-
+        
         self.__save_data(data)
 
-    def ftp_dir(self, path):
-        ftp = ftplib.FTP(self.__url)
-        ftp.login(self.__username, self.__password)
+        return data
 
-        ftp.cwd(path)
+    def __ftp_dir(self, path):
+        self.__ftp.login(self.__username, self.__password)
+
+        self.__ftp.cwd(path)
 
         data = []
 
-        ftp.dir(data.append)
+        self.__ftp.dir(data.append)
 
-        ftp.quit()
+        self.__ftp.quit()
 
         self.__print_data(data)
 
 
 if __name__ == '__main__':
     nordpool = NordpoolAPI()
+
 
     # # path and file that works with regular decoding
     # path = "/Operating_data/Denmark"
@@ -91,5 +139,21 @@ if __name__ == '__main__':
     path = "/Elspot/Elspot_prices/Denmark/Denmark_West"
     file = "odedkk22.sdv"
 
-    nordpool.ftp_retrieve(path, file)
-    # nordpool.ftp_dir(path)
+    # print("hello")
+    data = nordpool.ftp_retrieve(path, file)
+
+
+
+    # # check each line below the headers has more than 35 columns
+    # for line in data:
+    #     for character in line:
+    #         if character == ";":
+    #             column_count += 1
+    #     print(column_count)
+    #     if line_count > 5 and column_count < 35:# 35:
+    #         is_ok = False 
+    #     column_count = 0
+    #     line_count += 1
+
+
+    # nordpool.__ftp_dir(path)
