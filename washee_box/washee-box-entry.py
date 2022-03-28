e6c51dd6898020897bd9830a00dcf15645311b41
MartinBruun <mbm93fen@gmail.com>
2022-03-28 10:50:05 +0200

    Add requirements

--- washee_box/washee-box-entry.py
+++ washee_box/washee-box-entry.py
@@ -173,5 +173,8 @@ if __name__ == "__main__":
         "BOX_DEBUG", default="0")) == 0 else True
     port = int(os.environ.get("BOX_PORT", default="8001"))
     host = os.environ.get("BOX_HOST", default="0.0.0.0")
+    print(debug)
+    print(os.environ.get("BOX_DEBUG", default="-5"))
+    print(os.environ.get("MAX_WASHINGTIME_IN_SEC",default="-10"))
     app.run(debug=debug, port=port, host=host)
     # app.run(debug=True, port=5000, host='0.0.0.0')
