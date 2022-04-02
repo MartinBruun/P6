3387d789f4659c1e32a97e63bf70c93e90ea7de3
saxjax <saxjax@saxjax.dk>
2022-04-02 15:56:25 +0200

    the daynumber is encircled if it is today, that is if it is the 3rd every the 3.rd are encircled, tweek the getFieldNumberDate and the isToday functions, and make a listbuilder for the calendarCard daycards

--- washee_app/lib/core/widgets/day_card.dart
+++ washee_app/lib/core/widgets/day_card.dart
@@ -3,65 +3,81 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DayCard extends StatelessWidget {
   final String dayName;
-  final int date;
+  final int dayNumber;
   final VoidCallback selectHandler;
-  final int greenness; //-1 is neutral , 0 is red 10 is green
+  final int greenness;
+  final bool isToday;
+  final bool isSelected; //-1 is neutral , 0 is red 10 is green
 
-  const DayCard(
-      {required this.selectHandler,
-      required this.date,
-      required this.dayName,
-      required this.greenness})
-      : super();
+  const DayCard({
+    required this.selectHandler,
+    required this.dayNumber,
+    required this.dayName,
+    required this.greenness,
+    required this.isToday,
+    required this.isSelected,
+  }) : super();
 
   Color greennessColor({bool lighten = false}) {
-     if (date == 0) {
+    if (dayNumber == 0) {
       return lighten ? Colors.transparent : Colors.transparent;
     } else if (greenness == -1) {
       return lighten ? Colors.white38 : Colors.blueGrey;
     } else if (greenness > -1 && greenness < 3) {
       return lighten ? Colors.lightBlue : Colors.red;
     } else if (greenness >= 3 && greenness < 5) {
-      return lighten ? Colors.lightBlue :  Color.fromARGB(255, 200, 218, 40);
+      return lighten ? Colors.lightBlue : Color.fromARGB(255, 200, 218, 40);
     } else if (greenness >= 5) {
-      return lighten ? Colors.lightBlue :  Colors.green;
+      return lighten ? Colors.lightBlue : Colors.green;
     }
-    return  lighten ? Colors.white24 :  Colors.black;
+    return lighten ? Colors.white24 : Colors.black;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: greennessColor(lighten:true),
+      color: greennessColor(lighten: true),
       width: 0.13.sw,
       margin: EdgeInsets.all(0.0005.sw),
       padding: EdgeInsets.all(1),
-      child: date == 0 ? Text(" ") : ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: greennessColor(),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(0.1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              Text(
-                '$date',
-                style: TextStyle(fontSize: 10),
+      child: dayNumber == 0
+          ? Text(" ")
+          : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: greennessColor(),
               ),
-              Text(
-                dayName,
-                style: TextStyle(fontSize: 8),
-                textAlign: TextAlign.justify,
-                softWrap: false,
-                overflow: TextOverflow.visible,
+              child: Padding(
+                padding: const EdgeInsets.all(0.1),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [ 
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border:Border.all(color:isToday ? Colors.red : Colors.transparent,width:2,)
+                      ),
+                      child: 
+                      Text(
+                        isSelected ? '$dayNumber X ': '$dayNumber',
+                        style: TextStyle(fontSize: 10),
+                        textAlign: TextAlign.justify,
+                        softWrap: false,
+                        overflow: TextOverflow.visible,
+
+                      ),
+                    ),
+                    Text(
+                      dayName,
+                      style: TextStyle(fontSize: 8),
+                      textAlign: TextAlign.justify,
+                      softWrap: false,
+                      overflow: TextOverflow.visible,
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-        onPressed: selectHandler,
-      ),
+              onPressed: selectHandler,
+            ),
     );
   }
 }
