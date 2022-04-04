7f98e26cbd1a13df902537ba51701f0191535e9f
saxjax <saxjax@saxjax.dk>
2022-04-04 15:59:50 +0200

    a timeselector, with selections for vask tør, fromtime,totime, a really bad slider for selecting the time, a bad gradient for displaying the greenscore at different times.

--- washee_app/lib/core/widgets/TimeSelectorCard.dart
+++ washee_app/lib/core/widgets/TimeSelectorCard.dart
@@ -1,15 +1,44 @@
+import 'package:dartz/dartz_unsafe.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TimeSelectorCard extends StatefulWidget {
-  const TimeSelectorCard({Key? key}) : super(key: key);
+  const TimeSelectorCard({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<TimeSelectorCard> createState() => _TimeSelectorCardViewState();
 }
 
 class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
+  List<int> greenScore = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    4
+  ]; //TODO: denne skal med som parameter i initializeren
   List<bool> _selections = [true, false];
+  DateTime fromTime = DateTime.now();
+  int bookingIntervalInMinutes = 150;
+  late DateTime toTime =
+      fromTime.add(Duration(minutes: bookingIntervalInMinutes));
+
+  double _currentSliderValue = DateTime.now().hour.toDouble();
+  //Todo: make a better gradient calculation
+  late List<Color> greenScoreDayArray = List.generate(
+      12,
+      (index) => Color.fromARGB(
+          255, greenScore[index] * 10, greenScore[index] * 10, 2));
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,7 +56,8 @@ class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
                       children: [
                         Icon(
                           Icons.water_drop,
-                          semanticLabel: "Vask",size: 40,
+                          semanticLabel: "Vask",
+                          size: 40,
                         ),
                         Text(
                           'Vask',
@@ -40,9 +70,11 @@ class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          Icon(Icons.air, 
-                          semanticLabel: "Tør",
-                          size: 40,),
+                          Icon(
+                            Icons.air,
+                            semanticLabel: "Tør",
+                            size: 40,
+                          ),
                           Text(
                             'Tør',
                             style: timeLabelStyle,
@@ -61,7 +93,6 @@ class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
                         _selections[1] = !_selections[1];
                       });
                     })),
-            
             Column(
               children: [
                 Container(
@@ -79,28 +110,65 @@ class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
                   child: Column(
                     children: [
                       Center(
-              child: Row(children: [
-                Spacer(),
-                Text(
-                  "fra ",
-                  style: timeLabelStyle,
-                ),
-                Spacer(),
-                Text("til", style: timeLabelStyle),
-                Spacer()
-              ]),
-            ),
+                        child: Row(children: [
+                          Spacer(),
+                          Text(
+                            "fra ",
+                            style: timeLabelStyle,
+                          ),
+                          Spacer(),
+                          Text("til", style: timeLabelStyle),
+                          Spacer()
+                        ]),
+                      ),
                       Row(children: [
                         Spacer(),
                         Padding(
                             padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                            child: Text(" 12:35 ", style: timeNumberStyle)),
+                            child: Text(
+                                " ${fromTime.hour} : ${fromTime.minute}  ",
+                                style: timeNumberStyle)),
                         Spacer(),
                         Padding(
                             padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                            child: Text(" 14:35 ", style: timeNumberStyle)),
+                            child: Text(" ${toTime.hour} : ${toTime.minute}  ",
+                                style: timeNumberStyle)),
                         Spacer()
                       ]),
+                      Container(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [...greenScoreDayArray]),
+                            border:
+                                Border.all(width: 4, color: Colors.white12)),
+                        child: Slider(
+                            min: 1,
+                            max: 25,
+                            divisions: 360, //minute on 24 hours
+                            label: _currentSliderValue.ceil().toString(),
+                            value: _currentSliderValue,
+                            onChanged: (double value) {
+                              int hour = _currentSliderValue.ceil();
+                              int minute =
+                                  (60 / _currentSliderValue - hour).ceil();
+                              var from = DateTime(fromTime.year, fromTime.month,
+                                  fromTime.day, hour, minute);
+                              var to = DateTime(
+                                  fromTime.year,
+                                  fromTime.month,
+                                  fromTime.day,
+                                  hour,
+                                  minute + bookingIntervalInMinutes);
+
+                              setState(() {
+                                _currentSliderValue = value;
+                                fromTime = from;
+                                toTime = to;
+                              });
+                            }),
+                      )
                     ],
                   ),
                 ),
@@ -120,9 +188,8 @@ TextStyle timeLabelStyle = TextStyle(
 );
 
 TextStyle timeNumberStyle = TextStyle(
-  color: Color.fromARGB(255, 209, 255, 59),
-  backgroundColor: Colors.white30,
-  fontWeight: FontWeight.bold,
-  fontSize: 40,
-  shadows: [Shadow(offset: Offset( 1.0,1.0),color: Colors.black)]
-);
+    color: Color.fromARGB(255, 209, 255, 59),
+    backgroundColor: Colors.white30,
+    fontWeight: FontWeight.bold,
+    fontSize: 40,
+    shadows: [Shadow(offset: Offset(1.0, 1.0), color: Colors.black)]);
