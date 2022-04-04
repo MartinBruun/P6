8becc7c60deada6cbb282253c6f1b911b8bed162
MartinBruun <mbm93fen@gmail.com>
2022-04-04 02:20:04 +0200

    Add getAllBookings method to BookLaundryRemoteImpl

--- washee_app/lib/features/booking/data/datasources/book_remote.dart
+++ washee_app/lib/features/booking/data/datasources/book_remote.dart
@@ -1,8 +1,16 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:washee/core/booking/booking_model.dart';
+import 'package:washee/core/errors/exception_handler.dart';
 
-const String ENDPOINT = "blabla";
+const String ENDPOINT = "http://localhost:8000/api/1/bookings";
+
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+}
 
 abstract class BookRemote {
+  Future<List<BookingModel>> getAllBookings();
   Future<bool> book();
   Future<bool> pay();
 }
@@ -10,6 +18,19 @@ abstract class BookRemote {
 class BookLaundryRemoteImpl implements BookRemote {
   final http.Client client;
   BookLaundryRemoteImpl({required this.client});
+
+  @override
+  Future<List<BookingModel>> getAllBookings() async {
+    List<BookingModel> allBookings = [];
+    final response = await http.get(Uri.parse(ENDPOINT));
+    if (response.statusCode == 200) {
+      allBookings = (jsonDecode(response.body) as List).map((i) => BookingModel.fromJson(i)).toList();
+    }
+    else{
+      ExceptionHandler().handle("Did not receive All Bookings from backend, but received statusCode: " + response.statusCode.toString(), log:true);
+    }
+    return allBookings;
+  }
 
   @override
   Future<bool> book() async {
