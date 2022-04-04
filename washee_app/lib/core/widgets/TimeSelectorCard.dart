a623ce36a4c308e3f5b92639a558d98486d26c34
saxjax <saxjax@saxjax.dk>
2022-04-04 13:49:12 +0200

    added icons to select vask or tør

--- washee_app/lib/core/widgets/TimeSelectorCard.dart
+++ washee_app/lib/core/widgets/TimeSelectorCard.dart
@@ -9,50 +9,110 @@ class TimeSelectorCard extends StatefulWidget {
 }
 
 class _TimeSelectorCardViewState extends State<TimeSelectorCard> {
+  List<bool> _selections = [true, false];
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color:Colors.white24,
-      decoration: BoxDecoration(border:Border.all(width:4,color:Colors.white12)),
+        // color:Colors.white24,
+        decoration:
+            BoxDecoration(border: Border.all(width: 4, color: Colors.white12)),
         child: Column(
-      children: [
-        Center(child: Text(
-              "Vask ",
-              style: timeTitleStyle,
-            )),
-        Center(
-          child: Row(children: [
-            Spacer(),
-            Text(
-              "fra ",
-              style: timeLabelStyle,
+          children: [
+            Center(
+                child: ToggleButtons(
+                    children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.water_drop,
+                          semanticLabel: "Vask",size: 40,
+                        ),
+                        Text(
+                          'Vask',
+                          style: timeLabelStyle,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Icon(Icons.air, 
+                          semanticLabel: "Tør",
+                          size: 40,),
+                          Text(
+                            'Tør',
+                            style: timeLabelStyle,
+                          ),
+                        ],
+                      )),
+                ],
+                    color: Colors.white30,
+                    selectedColor: Colors.green,
+                    disabledColor: Colors.red,
+                    disabledBorderColor: Colors.red,
+                    isSelected: _selections,
+                    onPressed: (int index) {
+                      setState(() {
+                        _selections[0] = !_selections[0];
+                        _selections[1] = !_selections[1];
+                      });
+                    })),
+            
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.black38,
+                            Colors.black45,
+                            Colors.white38,
+                            Colors.black54,
+                          ]),
+                      border: Border.all(width: 4, color: Colors.white12)),
+                  child: Column(
+                    children: [
+                      Center(
+              child: Row(children: [
+                Spacer(),
+                Text(
+                  "fra ",
+                  style: timeLabelStyle,
+                ),
+                Spacer(),
+                Text("til", style: timeLabelStyle),
+                Spacer()
+              ]),
             ),
-            Spacer(),
-            Text("til",style: timeLabelStyle),
-            Spacer()
-          ]),
-        ),
-        Container(
-          decoration: BoxDecoration(border:Border.all(width:4,color:Colors.white12)),
-          child: Row(children: [
-            Spacer(),
-            Text("12:35", style:timeNumberStyle),
-            Spacer(),
-            Text("14:35", style:timeNumberStyle),
-            Spacer()
-          ]),
-        )
-      ],
-    ));
+                      Row(children: [
+                        Spacer(),
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                            child: Text(" 12:35 ", style: timeNumberStyle)),
+                        Spacer(),
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                            child: Text(" 14:35 ", style: timeNumberStyle)),
+                        Spacer()
+                      ]),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
 
-TextStyle timeTitleStyle = TextStyle(
-  color: Colors.white,
-  fontSize: 30,
-  fontWeight: FontWeight.bold
-  
-);
+TextStyle timeTitleStyle =
+    TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold);
 
 TextStyle timeLabelStyle = TextStyle(
   color: Colors.white,
@@ -60,8 +120,9 @@ TextStyle timeLabelStyle = TextStyle(
 );
 
 TextStyle timeNumberStyle = TextStyle(
-  color: Colors.yellow,
+  color: Color.fromARGB(255, 209, 255, 59),
   backgroundColor: Colors.white30,
   fontWeight: FontWeight.bold,
-  fontSize: 24,
+  fontSize: 40,
+  shadows: [Shadow(offset: Offset( 1.0,1.0),color: Colors.black)]
 );
