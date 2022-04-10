0acde9d08dcd5d9f25a88848a8f205bdbf7dd505
mkju <mkju19@student.aau.dk>
2022-04-10 02:32:37 +0200

    added widget for log in button

---
+++ washee_app/lib/features/Account/presentation/log_in_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:washee/core/presentation/themes/colors.dart';
+import 'package:washee/core/presentation/themes/dimens.dart';
+import 'package:washee/core/presentation/themes/themes.dart';
+import 'package:washee/features/Account/presentation/log_in_screen.dart';
+
+class LogInButton extends StatefulWidget {
+  const LogInButton({ Key? key }) : super(key: key);
+
+  @override
+  State<LogInButton> createState() => _LogInButtonState();
+}
+
+class _LogInButtonState extends State<LogInButton> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(0.005),
+      child: Container(
+        height: 70,
+        width: 200,
+        child: ElevatedButton(
+          child: Text("Log ind",
+          style: textStyle.copyWith(
+            fontSize: textSize_54,
+            fontWeight: FontWeight.w600
+          ),),
+          style: ElevatedButton.styleFrom(
+            primary: AppColors.deepGreen,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
+            )
+          ),
+          onPressed: () {
+            _openLogInScreen();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _openLogInScreen() async {
+    Map info = await Navigator.push(context, MaterialPageRoute(builder: (context) => const LogInScreen()));
+  }
+
+}
