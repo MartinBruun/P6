53ec179db14f3e06bf8a875fb767de75646fe53e
Jakob Skov Søndergård <saxjax@saxjax.dk>
2022-04-11 14:49:23 +0200

    Fixing appfor production (#120)
    * first commit
    
    * refactors and adjusts the calendar view UI
    
    * modifies the day_card
    
    * displays day of the week
    
    * adds the greenscore color and a timepicker
    
    * removes outcommented code
    
    * cleans up the code and adds a few comments
    
    * chmod has been run on web down and up scripts
    
    * updating the scheme i n xcode to run release when archiving
    tarting
    
    :q
    
    * changed environment to default to production
    
    Co-authored-by: Rallefar1103 <rasmus@turftank.com>
    Co-authored-by: MartinBruun <mbm93fen@gmail.com>

---
+++ washee_app/lib/features/booking/presentation/provider/calendar_provider.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:washee/core/helpers/date_helper.dart';
+
+class CalendarProvider extends ChangeNotifier {
+  bool _isLoadingDays = false;
+  final List<int> _numberOfFields = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31
+  ];
+
+  final List<int> _greenScoreData = [
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    1,
+    -1,
+    10,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    2,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+  ];
+
+  Map<String, List<DateTime>> daysInMonthMap = {};
+
+  // Fills up the daysInMonthMap with the respective number of dates for the given month
+  getDaysInBetween(DateTime startDate) {
+    var endDate = DateTime(startDate.year, startDate.month + 1, 0);
+
+    daysInMonthMap[_dateHelper.monthName(startDate.month)] = [];
+
+    if (daysInMonthMap[_dateHelper.monthName(startDate.month)] != null) {
+      for (int i = 0; i <= endDate.difference(startDate).inDays; i++) {
+        daysInMonthMap['${_dateHelper.monthName(startDate.month)}']!
+            .add(startDate.add(Duration(days: i)));
+      }
+    } else {
+      print(
+          "NULL!!! - Month name is: ${_dateHelper.monthName(startDate.month)}");
+    }
+  }
+
+  DateHelper _dateHelper = DateHelper();
+  bool _isSelected = false;
+
+  bool get isLoadingDays => _isLoadingDays;
+
+  List<int> get numberOfFields => _numberOfFields;
+
+  List<int> get greenScoreData => _greenScoreData;
+
+  DateHelper get dateHelper => _dateHelper;
+
+  bool get isSelected => _isSelected;
+
+  set isLoadingDays(bool value) {
+    _isLoadingDays = value;
+    notifyListeners();
+  }
+
+  set isSelected(bool value) {
+    _isSelected = value;
+    notifyListeners();
+  }
+
+  // int getGreenScore(int field, DateTime date) {
+  //   DateTime tempFieldDate = _dateHelper.getFieldNumberDate(field, date);
+  //   return tempFieldDate == null ? -1 : greenScoreData[tempFieldDate.day - 1];
+  // }
+
+  int getGreenScore(DateTime date, int daysInCurrentMonth) {
+    if (date.day <= daysInCurrentMonth) {
+      return greenScoreData[date.day - 1];
+    } else {
+      return 0;
+    }
+  }
+}
---
+++ washee_app/lib/features/booking/presentation/widgets/day_card.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:washee/core/helpers/date_helper.dart';
+import 'package:washee/core/presentation/themes/colors.dart';
+import 'package:washee/core/presentation/themes/dimens.dart';
+import 'package:washee/core/presentation/themes/themes.dart';
+
+class DayCard extends StatefulWidget {
+  final int greenScore;
+  final int dayNumber;
+  final String dayName;
+  final DateTime currentDate;
+
+  DayCard({
+    required this.greenScore,
+    required this.dayNumber,
+    required this.dayName,
+    required this.currentDate,
+  });
+
+  @override
+  State<DayCard> createState() => _DayCardState();
+}
+
+class _DayCardState extends State<DayCard> {
+  Color greenScoreColor({bool lighten = false}) {
+    if (widget.greenScore == 0) {
+      return lighten ? Colors.transparent : Colors.transparent;
+    } else if (widget.greenScore == -1) {
+      return lighten ? Colors.white38 : AppColors.borderMiddleGray;
+    } else if (widget.greenScore > -1 && widget.greenScore < 3) {
+      return lighten ? Colors.lightBlue : Colors.red;
+    } else if (widget.greenScore >= 3 && widget.greenScore < 5) {
+      return lighten ? Colors.lightBlue : Color.fromARGB(255, 200, 218, 40);
+    } else if (widget.greenScore >= 5) {
+      return lighten ? Colors.lightBlue : Colors.green;
+    }
+    return lighten ? Colors.white24 : Colors.black;
+  }
+
+  bool _isToday = false;
+  DateHelper helper = DateHelper();
+
+  @override
+  void initState() {
+    _isToday = helper.isToday(widget.currentDate);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black12,
+      width: 0.13.sw,
+      margin: EdgeInsets.all(0.0005.sw),
+      padding: EdgeInsets.all(1),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: greenScoreColor(),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(0.1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 50.h,
+                height: 50.h,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _isToday ? Colors.blue : Colors.transparent),
+                child: Center(
+                  child: Text(
+                    '${widget.dayNumber.toString()}',
+                    style: textStyle.copyWith(
+                        fontSize: textSize_20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                    textAlign: TextAlign.justify,
+                    softWrap: false,
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+              ),
+              Text(
+                helper.translateDayName(widget.dayName),
+                style: textStyle.copyWith(
+                  fontSize: textSize_15,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.justify,
+                softWrap: false,
+                overflow: TextOverflow.visible,
+              ),
+            ],
+          ),
+        ),
+        onPressed: () {
+          showTimePicker(context: context, initialTime: TimeOfDay.now());
+        },
+      ),
+    );
+  }
+}
