46e0144272c67b21177998ef4bd6ba44704b87d8
MartinBruun <58165955+MartinBruun@users.noreply.github.com>
2022-04-11 11:24:25 +0200

    Working environment, implemented in BoxCommunicator (#118)

---
+++ washee_app/lib/core/environments/base_config.dart
@@ -0,0 +1,6 @@
+abstract class BaseConfig {
+  // Vars for the backend
+  String get webApiHost;
+  // Vars for the box
+  String get boxApiHost;
+}
--- washee_app/lib/main.dart
+++ washee_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:washee/core/environments/environment.dart';
 import 'package:washee/core/pages/home_screen.dart';
 import 'package:washee/core/providers/global_provider.dart';
 import 'package:washee/features/unlock/presentation/provider/unlock_provider.dart';
@@ -8,6 +9,11 @@ import 'injection_container.dart' as ic;
 import 'core/presentation/themes/themes.dart';
 
 void main() async {
+  const String environment = String.fromEnvironment(
+    'ENVIRONMENT',
+    defaultValue: Environment.DEV,
+  );
+  Environment().initConfig(environment);
   WidgetsFlutterBinding.ensureInitialized();
   ic.initAll();
   runApp(WasheeApp());
