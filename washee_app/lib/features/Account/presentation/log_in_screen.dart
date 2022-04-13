3fcb449f14074c8776f618fe50b08d9d6cbad14f
mkju <mkju19@student.aau.dk>
2022-04-13 22:20:53 +0200

    initiate a user on the device during login

--- washee_app/lib/features/Account/presentation/log_in_screen.dart
+++ washee_app/lib/features/Account/presentation/log_in_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:washee/core/account/account.dart';
 import 'package:washee/core/presentation/themes/colors.dart';
 import 'package:washee/core/presentation/themes/dimens.dart';
 import 'package:washee/core/presentation/themes/themes.dart';
+import 'package:washee/core/account/user.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({ Key? key }) : super(key: key);
@@ -25,16 +27,17 @@ class _LogInScreenState extends State<LogInScreen> {
 
   void _logIn(){
     print("attempting to log in");
-    print("username is " + usernameController.text);
-    print("password is " + passwordController.text);
+    ActiveUser user = ActiveUser();
+    user.initUser(1, "mail@mail", usernameController.text, Account(id: 1, name: "name", balance: 10));
+    
     // showDialog(context: context, builder: (builder) {
     //   return AlertDialog(
     //     content: Text(usernameController.text),
     //   );
     // }
     // );
-    bool loggedIn = true;
-    Navigator.pop(context, {"username": usernameController.text, "loggedIn": loggedIn});
+    //bool loggedIn = true;
+    Navigator.pop(context);
   }
 
   @override
