67bf0ddc3d8bcbeda6d76ded983dbd3d2f76ac03
MartinBruun <58165955+MartinBruun@users.noreply.github.com>
2022-04-14 11:37:16 +0200

    Update web scripts (#126)
    * Update web scripts
    
    * Make easier to use

--- scripts/web/prod_up.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-cd ..
-call docker-compose -f docker-compose.web.yml up -d --build
-call docker-compose -f docker-compose.web.yml exec web python3 manage.py migrate --noinput
-call docker-compose -f docker-compose.web.yml exec web python3 manage.py collectstatic --no-input
-cd scripts
-cd web
-echo You now run a production build on http://localhost/
-echo Remember to run prod_down.bat to safely close the containers and persist data
