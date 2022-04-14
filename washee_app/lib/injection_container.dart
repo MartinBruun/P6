a19bfcc336d7a409c1f0d4fb3f1f0c8f65dc2c2f
Rallefar1103 <rasmus@turftank.com>
2022-04-14 09:46:43 +0200

    updates the injection container

--- washee_app/lib/injection_container.dart
+++ washee_app/lib/injection_container.dart
@@ -28,6 +28,7 @@ void initAll() {
   initCoreAndExternal();
   initUnlock();
   initGetMachines();
+  initSignIn();
 }
 
 initCoreAndExternal() {
@@ -36,9 +37,6 @@ initCoreAndExternal() {
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton(() => Connectivity());
 
-  sl.registerLazySingleton<Authorizer>(() => AuthorizerImpl(dio: sl()));
-  sl.registerLazySingleton<WebCommunicator>(
-      () => WebCommunicatorImpl(dio: sl(), authorizer: sl()));
   sl.registerLazySingleton<BoxCommunicator>(
       () => BoxCommunicatorImpl(dio: sl()));
   sl.registerLazySingleton(() => Dio());
