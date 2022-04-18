ade29e4cdd3bd65c6e54c981a302930ffbe6a095
mkju19 <mkju19@student.aau.dk>
2022-04-18 11:26:58 +0200

    changed imports to use core widget

--- washee_app/lib/features/sign_in/presentation/pages/wrong_input.dart
+++ washee_app/lib/features/sign_in/presentation/pages/wrong_input.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:washee/features/sign_in/presentation/widgets/dialog_box_Ok.dart';
+import 'package:washee/core/widgets/dialog_box_Ok.dart';
 
 class IncorrectInput extends StatelessWidget {
-  const IncorrectInput({ Key? key }) : super(key: key);
+  const IncorrectInput({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Center(
-        child: DialogBoxOk("Ugyldig email eller kodeord indtastet")
-      ),
+      body: Center(child: DialogBoxOk("Ugyldig email eller kodeord indtastet")),
     );
   }
 }
