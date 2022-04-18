5753a44f95b791c7e1c283f9328bfb78251f3d2e
MartinBruun <mbm93fen@gmail.com>
2022-04-18 10:48:58 +0200

    Fix tests for Machine.fromJson

--- washee_app/test/features/get_machines/get_machines_test.dart
+++ washee_app/test/features/get_machines/get_machines_test.dart
@@ -7,7 +7,7 @@ import 'package:washee/features/get_machines/data/repositories/get_machines_repo
 
 import '../../fixtures/fixture_reader.dart';
 import '../unlock/data/repositories/unlock_repo_impl_test.mocks.dart';
-import 'get_machines_test.mocks.dart';
+import 'get_machines_test.mock_web.dart';
 
 @GenerateMocks([], customMocks: [
   MockSpec<BoxCommunicator>(
@@ -19,17 +19,53 @@ void main() {
     () async {
       // arrange
       var networkInfo = MockNetworkInfo();
-      var communicator = MockCommunicator();
+      var communicator = MockWebCommunicator();
       var repository = GetMachinesRepositoryImpl(
           communicator: communicator, networkInfo: networkInfo);
 
-      var string = fixture("machine_list.json");
-      var stringAsJson = json.decode(string);
+      var mockMachinesAsJson = listOfMockMachines();
       // act
-      final result = repository.constructMachineList(stringAsJson);
+      final result = repository.constructMachineList(mockMachinesAsJson);
 
       // assert
       expect(result.length, 4);
     },
   );
+}
+
+List<Map<String,dynamic>> listOfMockMachines(){
+  return [
+        {
+            "endTime": "null",
+            "machineID": "l1",
+            "machineType": "laundrymachine",
+            "name": "vaskemaskine 1",
+            "pin": 17,
+            "startTime": "null"
+        },
+        {
+            "endTime": "2022-03-07T10:15:26",
+            "machineID": "l2",
+            "machineType": "laundrymachine",
+            "name": "vaskemaskine 2",
+            "pin": 27,
+            "startTime": "2022-03-07T08:15:26"
+        },
+        {
+            "endTime": "2022-03-07T10:15:26",
+            "machineID": "t1",
+            "machineType": "dryermachine",
+            "name": "tørretumbler 1",
+            "pin": 23,
+            "startTime": "2022-03-07T08:15:26"
+        },
+        {
+            "endTime": "2022-03-07T10:15:26",
+            "machineID": "t2",
+            "machineType": "dryermachine",
+            "name": "tørretumbler 2",
+            "pin": 12,
+            "startTime": "2022-03-07T08:15:26"
+        }
+    ];
 }
