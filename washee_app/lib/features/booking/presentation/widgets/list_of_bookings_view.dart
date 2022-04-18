9abc5ab16f368dd7d5f00a6bfd7129a29cf94c20
mkju19 <mkju19@student.aau.dk>
2022-04-18 13:38:33 +0200

    added provider for deleting bookings

--- washee_app/lib/features/booking/presentation/widgets/list_of_bookings_view.dart
+++ washee_app/lib/features/booking/presentation/widgets/list_of_bookings_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:washee/core/presentation/themes/dimens.dart';
 import 'package:washee/core/presentation/themes/themes.dart';
+import 'package:washee/features/booking/domain/usecases/delete_booking.dart';
+import 'package:washee/features/booking/presentation/provider/booking_provider.dart';
 import 'package:washee/features/user_info/presentation/widgets/booking_info.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:washee/injection_container.dart';
 import '../../data/models/booking_model.dart';
 
 class ListOfBookingsView extends StatelessWidget {
@@ -22,7 +26,15 @@ class ListOfBookingsView extends StatelessWidget {
           Padding(
             padding: EdgeInsets.only(bottom: 20.h),
             child: ElevatedButton(
-              onPressed: null,
+              onPressed: () async {
+                await sl<DeleteBookingUseCase>().call(await DeleteBookingParams(
+                    bookingID: bookings[index].bookingID!));
+
+                var booking =
+                    Provider.of<BookingProvider>(context, listen: false);
+
+                booking.refreshBookings();
+              },
               child: Text(
                 "slet",
                 style: textStyle.copyWith(
