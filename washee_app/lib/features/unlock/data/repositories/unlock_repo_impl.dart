17a21264a0786db1280af1b695301c03b11f9e59
mkju19 <mkju19@student.aau.dk>
2022-04-19 14:36:30 +0200

    removed prints

--- washee_app/lib/features/unlock/data/repositories/unlock_repo_impl.dart
+++ washee_app/lib/features/unlock/data/repositories/unlock_repo_impl.dart
@@ -13,9 +13,7 @@ class UnlockRepositoryImpl implements UnlockRepository {
   Future<MachineModel?> unlock(MachineModel machine, Duration duration) async {
     if (await networkInfo.isConnected) {
       var data = await remote.unlock(machine, duration);
-      print("UNLOCK IS FINE");
       MachineModel updatedMachine = _constructMachineFromResponse(data);
-      print("CONSTRUCT NOT FINE");
       return updatedMachine;
     } else {
       return null;
