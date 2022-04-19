a3003fa403fc757eac6f4b762286c53c375d3a4b
MartinBruun <mbm93fen@gmail.com>
2022-04-19 21:11:06 +0200

    Update

--- washee_app/lib/core/network/network_info.dart
+++ washee_app/lib/core/network/network_info.dart
@@ -75,7 +75,7 @@ class NetworkInfoImpl implements NetworkInfo {
           joinOnce: true,
           isHidden: false,
           security: NetworkSecurity.WPA);
-      WiFiForIoTPlugin.forceWifiUsage(true);
+      await WiFiForIoTPlugin.forceWifiUsage(true);
       print("Connected to BoxWifi from NetworkInfo");
       return result;
     } catch (e) {
@@ -93,7 +93,7 @@ class NetworkInfoImpl implements NetworkInfo {
   @override
   Future<bool> disconnectFromBoxWifi() async {
     try {
-      WiFiForIoTPlugin.forceWifiUsage(false);
+      await WiFiForIoTPlugin.forceWifiUsage(false);
       await WiFiForIoTPlugin.disconnect();
       return true;
     } catch (e) {
