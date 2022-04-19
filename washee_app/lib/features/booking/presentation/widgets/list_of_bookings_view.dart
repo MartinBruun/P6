7cc9da4aa333800d0c1ab6bf43e0593b426c7d02
Rallefar1103 <rasmus@turftank.com>
2022-04-19 15:23:28 +0200

    fixes that early bookings show up in "mine bookinger"

--- washee_app/lib/features/booking/presentation/widgets/list_of_bookings_view.dart
+++ washee_app/lib/features/booking/presentation/widgets/list_of_bookings_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:washee/core/helpers/date_helper.dart';
 import 'package:washee/core/presentation/themes/dimens.dart';
 import 'package:washee/core/presentation/themes/themes.dart';
 import 'package:washee/core/usecases/usecase.dart';
@@ -15,54 +16,92 @@ class ListOfBookingsView extends StatelessWidget {
   final List<BookingModel> bookings;
 
   ListOfBookingsView({required this.bookings});
+  var currentTime = DateHelper.currentTime();
+  bool _validateShowBooking(BookingModel bookingToShow, DateTime current) {
+    if (bookingToShow.endTime!.day == current.day) {
+      if (bookingToShow.endTime!.hour > current.hour) {
+        return true;
+      }
+      return false;
+    } else if (bookingToShow.endTime!.day > current.day) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (context, index) => Row(
-        children: [
-          BookingInfo(
-            bookingInfo: (bookings[index].machineResource.contains("/api/1/machines/1/") ? "Vask" : "Tørring") +
-                " " +
-                bookings[index].startTime!.year.toString() +
-                "/" +
-                bookings[index].startTime!.month.toString() +
-                "/" +
-                bookings[index].startTime!.day.toString() + 
-                " " +
-                bookings[index].startTime!.hour.toString().padLeft(2,"0") +
-                ":" +
-                bookings[index].startTime!.minute.toString().padLeft(2,"0") +
-                "-" +
-                bookings[index].endTime!.hour.toString().padLeft(2,"0") +
-                ":" +
-                bookings[index].endTime!.minute.toString().padLeft(2,"0")
-          ),
-          Spacer(),
-          Padding(
-            padding: EdgeInsets.only(bottom: 20.h),
-            child: ElevatedButton(
-              onPressed: () async {
-                await sl<DeleteBookingUseCase>().call(DeleteBookingParams(
-                    bookingID: bookings[index].bookingID!));
-                await sl<UpdateAccountUseCase>().call(NoParams());
+      itemBuilder: (context, index) {
+        return _validateShowBooking(bookings[index], currentTime)
+            ? Row(
+                children: [
+                  BookingInfo(
+                      bookingInfo: (bookings[index]
+                                  .machineResource
+                                  .contains("/api/1/machines/1/")
+                              ? "Vask"
+                              : "Tørring") +
+                          " " +
+                          bookings[index].startTime!.year.toString() +
+                          "/" +
+                          bookings[index].startTime!.month.toString() +
+                          "/" +
+                          bookings[index].startTime!.day.toString() +
+                          " " +
+                          bookings[index]
+                              .startTime!
+                              .hour
+                              .toString()
+                              .padLeft(2, "0") +
+                          ":" +
+                          bookings[index]
+                              .startTime!
+                              .minute
+                              .toString()
+                              .padLeft(2, "0") +
+                          "-" +
+                          bookings[index]
+                              .endTime!
+                              .hour
+                              .toString()
+                              .padLeft(2, "0") +
+                          ":" +
+                          bookings[index]
+                              .endTime!
+                              .minute
+                              .toString()
+                              .padLeft(2, "0")),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20.h),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await sl<DeleteBookingUseCase>().call(
+                            DeleteBookingParams(
+                                bookingID: bookings[index].bookingID!));
+                        await sl<UpdateAccountUseCase>().call(NoParams());
 
-                var booking =
-                    Provider.of<BookingProvider>(context, listen: false);
+                        var booking = Provider.of<BookingProvider>(context,
+                            listen: false);
 
-                booking.refreshBookings();
-              },
-              child: Text(
-                "slet",
-                style: textStyle.copyWith(
-                    fontSize: textSize_20, color: Colors.white),
-              ),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red)),
-            ),
-          )
-        ],
-      ),
+                        booking.refreshBookings();
+                      },
+                      child: Text(
+                        "slet",
+                        style: textStyle.copyWith(
+                            fontSize: textSize_20, color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red)),
+                    ),
+                  )
+                ],
+              )
+            : SizedBox.shrink();
+      },
       itemCount: bookings.length,
     );
   }
