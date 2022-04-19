fa8402467184acd9a84cc66ef8a38b9b710b544a
mkju19 <mkju19@student.aau.dk>
2022-04-19 12:15:16 +0200

    refactored yes button

--- washee_app/lib/features/unlock/presentation/widgets/initiate_wash_dialog.dart
+++ washee_app/lib/features/unlock/presentation/widgets/initiate_wash_dialog.dart
@@ -1,18 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:washee/core/errors/error_handler.dart';
-import 'package:washee/core/errors/http_error_prompt.dart';
-import 'package:washee/core/pages/home_screen.dart';
-import 'package:washee/core/pages/pages_enum.dart';
-import 'package:washee/core/usecases/usecase.dart';
+import 'package:provider/provider.dart';
 import 'package:washee/core/washee_box/machine_model.dart';
-import 'package:washee/features/sign_out/presentation/widgets/no_buttton.dart';
-import 'package:washee/features/unlock/domain/usecases/connect_box_wifi.dart';
-import 'package:washee/features/unlock/domain/usecases/disconnect_box_wifi.dart';
-import 'package:washee/features/unlock/domain/usecases/unlock.dart';
-import 'package:washee/features/unlock/presentation/widgets/wash_started_dialog.dart';
-import 'package:washee/injection_container.dart';
+import 'package:washee/features/unlock/presentation/provider/unlock_provider.dart';
+import 'package:washee/features/unlock/presentation/widgets/no_button.dart';
+import 'package:washee/features/unlock/presentation/widgets/yes_button.dart';
 
 import '../../../../core/presentation/themes/colors.dart';
 import '../../../../core/presentation/themes/dimens.dart';
@@ -30,19 +22,27 @@ class InitiateWashDialog extends StatefulWidget {
 
 class _InitiateWashDialogState extends State<InitiateWashDialog> {
   late MachineModel? fetchedMachine;
-  bool _isUnlockingMachine = false;
 
+  bool _isUnlockingMachine = false;
+  bool _startBooking = false;
   @override
   void initState() {
+    setState(() {
+      _startBooking = true;
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Center(child: _dialogBox(context)),
-    );
+    var unlockProvider = Provider.of<UnlockProvider>(context, listen: true);
+
+    return _startBooking || unlockProvider.isUnlocking
+        ? Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(child: _dialogBox(context)),
+          )
+        : SizedBox.shrink();
   }
 
   String _displayName() {
@@ -89,105 +89,14 @@ class _InitiateWashDialogState extends State<InitiateWashDialog> {
             children: [
               _isUnlockingMachine
                   ? CircularProgressIndicator()
-                  : _yesButton(context),
+                  : YesButton(
+                      machine: widget.machine,
+                    ),
               NoButton(),
             ],
           ),
         ],
       ),
     );
-  }
-
-  Widget _yesButton(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30.w),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              fixedSize: Size(254.w, 84.h),
-              primary: AppColors.deepGreen,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.h))),
-          child: Text(
-            'Ja',
-            style: textStyle.copyWith(
-              fontSize: textSize_32,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            ),
-          ),
-          onPressed: () async {
-            _unlockMachine(context);
-          },
-        ));
-  }
-
-  Future<void> _unlockMachine(BuildContext context) async {
-    setState(() {
-      _isUnlockingMachine = true;
-    });
-    try {
-      var result = kDebugMode
-          ? true
-          : await sl<ConnectBoxWifiUsecase>().call(NoParams());
-
-      if (result) {
-        fetchedMachine = await sl<UnlockUseCase>()
-            .call(UnlockParams(machine: widget.machine));
-        await sl<DisconnectBoxWifiUsecase>().call(NoParams());
-        if (fetchedMachine == null) {
-          setState(() {
-            _isUnlockingMachine = false;
-          });
-          ErrorHandler.errorHandlerView(
-              context: context,
-              prompt: HTTPErrorPrompt(
-                  message:
-                      "Det ser ud til, at du ikke har forbindelse til WasheeBox"));
-        } else {
-          print("From start_wash.dart: fetchedMachine went correctly!");
-          await sl<DisconnectBoxWifiUsecase>().call(NoParams());
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => HomeScreen(
-                page: PageNumber.WashScreen,
-              ),
-            ),
-          );
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return WashStartedDialog();
-            },
-          );
-        }
-      } else {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return HTTPErrorPrompt(
-                message:
-                    "Det ser ud til, at du ikke har forbindelse til WasheeBox. Forbind til boxen og prøv igen!");
-          },
-        );
-      }
-    } catch (e) {
-      await sl<DisconnectBoxWifiUsecase>().call(NoParams());
-      setState(() {
-        _isUnlockingMachine = false;
-      });
-      print(e.toString());
-      await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return HTTPErrorPrompt(
-              message:
-                  "Noget gik galt da vi forsøgte at låse maskinen op! Prøv venligst igen");
-        },
-      );
-    }
-    setState(() {
-      _isUnlockingMachine = false;
-    });
   }
 }
