b9f9ef00b7ff510359cf9a8eb1cf8e48bb739ef0
MartinBruun <mbm93fen@gmail.com>
2022-04-20 14:28:57 +0200

    Make booking not update when failing wifi connect

--- washee_app/lib/features/unlock/domain/usecases/unlock.dart
+++ washee_app/lib/features/unlock/domain/usecases/unlock.dart
@@ -24,17 +24,17 @@ class UnlockUseCase implements UseCase<MachineModel?, UnlockParams> {
     MachineModel? machine =
         await repository.unlock(params.machine, Duration(seconds: 10));
 
-    
-
-    // Bad practice, should be severely reorganized, when we have a proper understanding of the domain structure
-    // There is no strict seperation of Machine and Booking, giving these problems
-    List<Map<String, dynamic>> currentBooking = await sl<WebCommunicator>()
+    if (machine != null){
+      // Bad practice, should be severely reorganized, when we have a proper understanding of the domain structure
+      // There is no strict seperation of Machine and Booking, giving these problems
+      List<Map<String, dynamic>> currentBooking = await sl<WebCommunicator>()
         .getCurrentBookings(
             machineID: int.parse(params.machine.machineID.toString()),
             startTimeLessThan: DateHelper.currentTime(),
             endTimeGreaterThan: DateHelper.currentTime());
-    await sl<WebCommunicator>()
+      await sl<WebCommunicator>()
         .updateBooking(currentBooking[0]["id"].toString(), activated: true);
+    }
 
     return machine;
   }
