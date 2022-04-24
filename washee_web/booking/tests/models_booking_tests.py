59860552aee45bd5f3df0ef68cafa0a9c0a75d79
MartinBruun <mbm93fen@gmail.com>
2022-04-24 22:40:00 +0200

    Make better naming of tests

--- washee_web/booking/tests/models_booking_tests.py
+++ washee_web/booking/tests/models_booking_tests.py
@@ -19,7 +19,7 @@ def test_save_booking_deducts_money_from_service_from_account(create_booking_fun
     
 
 @pytest.mark.django_db
-def test_saving_and_deleting_booking_does_not_change_balance(create_booking_func):
+def test_saving_and_deleting_booking_does_not_change_balance_on_account(create_booking_func):
     price = 10
     balance = 30
     booking = create_booking_func(service_price=price, account_balance=balance)
