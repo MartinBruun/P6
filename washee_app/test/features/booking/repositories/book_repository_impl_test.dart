3c9f61cbc6705003a194b97e4d90ca7801024bc6
saxjax <saxjax@saxjax.dk>
2022-05-02 12:10:59 +0200

    refactored the booking tests

--- washee_app/test/features/booking/repositories/book_repository_impl_test.dart
+++ washee_app/test/features/booking/repositories/book_repository_impl_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:washee/core/network/network_info.dart';
 import 'package:washee/features/booking/data/datasources/book_remote.dart';
-import 'package:washee/features/booking/data/models/booking_entity.dart';
 import 'package:washee/features/booking/data/models/booking_model.dart';
 import 'package:washee/features/booking/data/repositories/book_repository_impl.dart';
 
@@ -13,92 +12,76 @@ class MockNetworkInfo extends Mock implements NetworkInfo {}
 class MockBookRemote extends Mock implements BookRemote {}
 
 void main() {
-  late BookRepositoryImpl sut_impl;
+  late BookRepositoryImpl sut_bookRepositoryImpl;
   late MockNetworkInfo mockNetworkInfo;
   late MockBookRemote mockRemote;
-  late Booking booking;
   late BookingModel mockBookingModel;
-  late int? accontID;
-  late bool? activated;
 
   setUp(() {
-    accontID = 12;
-    activated = false;
     mockRemote = MockBookRemote();
     mockNetworkInfo = MockNetworkInfo();
-    sut_impl =
+    sut_bookRepositoryImpl =
         BookRepositoryImpl(networkInfo: mockNetworkInfo, remote: mockRemote);
 
     var booking1_start_time = DateTime(2022, 01, 01, 2, 0);
-    booking = Booking(
+    mockBookingModel = BookingModel(
         bookingID: 12,
         startTime: booking1_start_time,
         machineResource: "https://mocked_machineResource/1",
         serviceResource: "https://mocked_serviceResource/1",
         accountResource: "https://mocked_accountResource/1");
-    mockBookingModel = BookingModel(
-        bookingID: booking.bookingID,
-        startTime: booking.startTime,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource);
   });
 
   test(
       'should verify that remote.postBooking() is called 1 times when isConnected is true',
       () async {
-    //check that postbooking is called 1 times
-    // check that remote.postBooking() is called 1 times
+   
     //arrange
     when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
     when(() => mockRemote.postBooking(
-            startTime: booking.startTime!,
-            machineResource: booking.machineResource,
-            serviceResource: booking.serviceResource,
-            accountResource: booking.accountResource))
+            startTime: mockBookingModel.startTime!,
+            machineResource: mockBookingModel.machineResource,
+            serviceResource: mockBookingModel.serviceResource,
+            accountResource: mockBookingModel.accountResource))
         .thenAnswer((_) async => mockBookingModel);
     //act
-    final result = await sut_impl.postBooking(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource);
+    final result = await sut_bookRepositoryImpl.postBooking(
+        startTime: mockBookingModel.startTime!,
+        machineResource: mockBookingModel.machineResource,
+        serviceResource: mockBookingModel.serviceResource,
+        accountResource: mockBookingModel.accountResource);
     // assert
-
     expect(result, mockBookingModel);
     verify(() => mockRemote.postBooking(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource)).called(1);
+        startTime: mockBookingModel.startTime!,
+        machineResource: mockBookingModel.machineResource,
+        serviceResource: mockBookingModel.serviceResource,
+        accountResource: mockBookingModel.accountResource)).called(1);
   });
 
   test(
       'should verify that remote.postBooking() is never called when isConnected is false',
       () async {
-    //check that postbooking is called 1 times
-    // check that remote.postBooking() is called 1 times
     //arrange
     when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
     when(() => mockRemote.postBooking(
-            startTime: booking.startTime!,
-            machineResource: booking.machineResource,
-            serviceResource: booking.serviceResource,
-            accountResource: booking.accountResource))
+            startTime: mockBookingModel.startTime!,
+            machineResource: mockBookingModel.machineResource,
+            serviceResource: mockBookingModel.serviceResource,
+            accountResource: mockBookingModel.accountResource))
         .thenAnswer((_) async => mockBookingModel);
     //act
-    final result = await sut_impl.postBooking(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource);
+    final result = await sut_bookRepositoryImpl.postBooking(
+        startTime: mockBookingModel.startTime!,
+        machineResource: mockBookingModel.machineResource,
+        serviceResource: mockBookingModel.serviceResource,
+        accountResource: mockBookingModel.accountResource);
     // assert
-
     expect(result, null);
     verifyNever(() => mockRemote.postBooking(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource));
+        startTime: mockBookingModel.startTime!,
+        machineResource: mockBookingModel.machineResource,
+        serviceResource: mockBookingModel.serviceResource,
+        accountResource: mockBookingModel.accountResource));
   });
 }
--- washee_app/test/features/booking/usecases/post_booking_test.dart
+++ washee_app/test/features/booking/usecases/post_booking_test.dart
@@ -13,79 +13,66 @@ class MockBookRepository extends Mock implements BookRepository {}
 void main() {
   late PostBookingUsecase sut_usecase;
   late MockBookRepository mockRepo;
-  late Booking booking;
   late BookingModel? mockBookingModel;
   late PostBookingParams params;
 
   setUp(() {
     mockRepo = MockBookRepository();
     sut_usecase = PostBookingUsecase(repository: mockRepo);
-    // create a user
-    var user = ActiveUser();
-    user.initUser(1, "test@test.test", "test", [
-      {'account_id': 1, 'name': "test_account", 'balance': 20.0}
-    ]);
+    
     // create a booking
     var booking1_start_time = DateTime(2022, 01, 01, 2, 0);
-    booking = Booking(
+    mockBookingModel = BookingModel(
         bookingID: 12,
         startTime: booking1_start_time,
         machineResource: "https://mocked_machineResource/1",
         serviceResource: "https://mocked_serviceResource/1",
         accountResource: "https://mocked_accountResource/1");
 
-    mockBookingModel = BookingModel(
-        bookingID: booking.bookingID,
-        startTime: booking.startTime,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource);
-
     params = PostBookingParams(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        accountResource: booking.accountResource,
-        serviceResource: booking.serviceResource);
+        startTime: mockBookingModel!.startTime!,
+        machineResource: mockBookingModel!.machineResource,
+        accountResource: mockBookingModel!.accountResource,
+        serviceResource: mockBookingModel!.serviceResource);
   });
 
   test(
       """should verify that 1 call has been made to 
       the repository BookRepository's postBooking method """,
       () async {
-    // arrange
 
+    // arrange
     when(() => mockRepo.postBooking(
-            startTime: booking.startTime!,
-            machineResource: booking.machineResource,
-            serviceResource: booking.serviceResource,
-            accountResource: booking.accountResource))
+            startTime: mockBookingModel!.startTime!,
+            machineResource: mockBookingModel!.machineResource,
+            serviceResource: mockBookingModel!.serviceResource,
+            accountResource: mockBookingModel!.accountResource))
         .thenAnswer((_) async => mockBookingModel);
     // act
     final result = await sut_usecase.call(params);
-
     // assert
     expect(result, mockBookingModel);
     verify(() => mockRepo.postBooking(
-        startTime: booking.startTime!,
-        machineResource: booking.machineResource,
-        serviceResource: booking.serviceResource,
-        accountResource: booking.accountResource)).called(1);
+        startTime: mockBookingModel!.startTime!,
+        machineResource: mockBookingModel!.machineResource,
+        serviceResource: mockBookingModel!.serviceResource,
+        accountResource: mockBookingModel!.accountResource)).called(1);
   });
 
 /// theese next two tests has to be run in turn first and second. they show that the setUp function is called between every test function.///
 /// 
   // test('Should verify that setUp is recalled with every test function', () {
-  //   booking.startTime = booking.startTime!.add( Duration(days:1));
+  //   mockBookingModel!.startTime = mockBookingModel!.startTime!.add( Duration(days:1));
     
   //   verifyNever(() => mockRepo.postBooking(
-  //       startTime: booking.startTime!.add( Duration(days:1)),
-  //       machineResource: booking.machineResource,
-  //       serviceResource: booking.serviceResource,
-  //       accountResource: booking.accountResource));
+  //       startTime: mockBookingModel!.startTime!.add( Duration(days:1)),
+  //       machineResource: mockBookingModel!.machineResource,
+  //       serviceResource: mockBookingModel!.serviceResource,
+  //       accountResource: mockBookingModel!.accountResource));
   // });
 
   // test('Test that setUp is called with every test function', () {
-  //     expect(booking.startTime,DateTime(2022, 01, 01, 2, 0) );
+  //     expect(mockBookingModel!.startTime,DateTime(2022, 01, 01, 2, 0) );
   // });
 
 
