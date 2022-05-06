d0226348058eaa10baab643c854729aeb86105dd
saxjax <saxjax@saxjax.dk>
2022-05-06 18:31:45 +0200

    please write the tests in calendar_provider_test so that they use the functions from calendar_provider

--- washee_app/test/features/washee_service/presentation/calendar_provider_test.dart
+++ washee_app/test/features/washee_service/presentation/calendar_provider_test.dart
@@ -87,7 +87,7 @@ void main() {
   });
 
   //__________________________________
-
+//TODO:important this should use the calculateBestGreenScore function from calendar_provider
   int calculateBestGreenScore(
       List<GreenScore> greenScoresForDay, List<DateTime> timeSlots) {
     int bestScore = -1;
@@ -127,115 +127,115 @@ void main() {
     return bestScore;
   }
 
-  var greenScoreList1 = [
-    GreenScore(hour: 06, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 06, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 07, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 07, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 08, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 08, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 09, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 09, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 10, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 10, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 11, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 11, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 12, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 12, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 13, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 13, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 14, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 14, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 15, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 15, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 16, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 16, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 17, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 17, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 18, minute: 00, greenScore: 4, vacant: true),
-    GreenScore(hour: 18, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 19, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 19, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 20, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 20, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 21, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 21, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 22, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 22, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 23, minute: 00, greenScore: 8, vacant: false),
-  ];
-  test(
-      'testing that the best availlable greenscore is returned from when star is booked',
-      () {
-    List<DateTime> _slots = [];
-    var startTime = DateTime(2022, 5, 1, 06, 0);
-    _slots.add(startTime);
-    for (int i = 0; i < 34; i++) {
-      startTime = startTime.add(Duration(minutes: 30));
-      _slots.add(startTime);
-    }
+  // var greenScoreList1 = [
+  //   GreenScore(hour: 06, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 06, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 07, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 07, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 08, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 08, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 09, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 09, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 10, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 10, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 11, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 11, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 12, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 12, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 13, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 13, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 14, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 14, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 15, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 15, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 16, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 16, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 17, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 17, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 18, minute: 00, greenScore: 4, vacant: true),
+  //   GreenScore(hour: 18, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 19, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 19, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 20, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 20, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 21, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 21, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 22, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 22, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 23, minute: 00, greenScore: 8, vacant: false),
+  // ];
+  // test(
+  //     'testing that the best availlable greenscore is returned from when star is booked',
+  //     () {
+  //   List<DateTime> _slots = [];
+  //   var startTime = DateTime(2022, 5, 1, 06, 0);
+  //   _slots.add(startTime);
+  //   for (int i = 0; i < 34; i++) {
+  //     startTime = startTime.add(Duration(minutes: 30));
+  //     _slots.add(startTime);
+  //   }
 
-    //act
-    var result = calculateBestGreenScore(greenScoreList1, _slots);
+  //   //act
+  //   var result = calculateBestGreenScore(greenScoreList1, _slots);
 
-    //assert
-    expect(result, 8);
-  });
+  //   //assert
+  //   expect(result, 8);
+  // });
 
-  var greenScoreList2 = [
-    GreenScore(hour: 06, minute: 00, greenScore: 8, vacant: true),
-    GreenScore(hour: 06, minute: 30, greenScore: 8, vacant: true),
-    GreenScore(hour: 07, minute: 00, greenScore: 8, vacant: true),
-    GreenScore(hour: 07, minute: 30, greenScore: 8, vacant: true),
-    GreenScore(hour: 08, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 08, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 09, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 09, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 10, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 10, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 11, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 11, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 12, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 12, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 13, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 13, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 14, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 14, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 15, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 15, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 16, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 16, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 17, minute: 00, greenScore: 1, vacant: true),
-    GreenScore(hour: 17, minute: 30, greenScore: 1, vacant: true),
-    GreenScore(hour: 18, minute: 00, greenScore: 4, vacant: true),
-    GreenScore(hour: 18, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 19, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 19, minute: 30, greenScore: 4, vacant: false),
-    GreenScore(hour: 20, minute: 00, greenScore: 4, vacant: false),
-    GreenScore(hour: 20, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 21, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 21, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 22, minute: 00, greenScore: 8, vacant: false),
-    GreenScore(hour: 22, minute: 30, greenScore: 8, vacant: false),
-    GreenScore(hour: 23, minute: 00, greenScore: 8, vacant: false),
-  ];
-  test(
-      'testing that the best availlable greenscore is returned when some slots are availlable in the beginning but not enough to form 6 slots.',
-      () {
-    List<DateTime> _slots = [];
-    var startTime = DateTime(2022, 5, 1, 06, 0);
-    _slots.add(startTime);
-    for (int i = 0; i < 34; i++) {
-      startTime = startTime.add(Duration(minutes: 30));
-      _slots.add(startTime);
-    }
+  // var greenScoreList2 = [
+  //   GreenScore(hour: 06, minute: 00, greenScore: 8, vacant: true),
+  //   GreenScore(hour: 06, minute: 30, greenScore: 8, vacant: true),
+  //   GreenScore(hour: 07, minute: 00, greenScore: 8, vacant: true),
+  //   GreenScore(hour: 07, minute: 30, greenScore: 8, vacant: true),
+  //   GreenScore(hour: 08, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 08, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 09, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 09, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 10, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 10, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 11, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 11, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 12, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 12, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 13, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 13, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 14, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 14, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 15, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 15, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 16, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 16, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 17, minute: 00, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 17, minute: 30, greenScore: 1, vacant: true),
+  //   GreenScore(hour: 18, minute: 00, greenScore: 4, vacant: true),
+  //   GreenScore(hour: 18, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 19, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 19, minute: 30, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 20, minute: 00, greenScore: 4, vacant: false),
+  //   GreenScore(hour: 20, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 21, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 21, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 22, minute: 00, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 22, minute: 30, greenScore: 8, vacant: false),
+  //   GreenScore(hour: 23, minute: 00, greenScore: 8, vacant: false),
+  // ];
+  // test(
+  //     'testing that the best availlable greenscore is returned when some slots are availlable in the beginning but not enough to form 6 slots.',
+  //     () {
+  //   List<DateTime> _slots = [];
+  //   var startTime = DateTime(2022, 5, 1, 06, 0);
+  //   _slots.add(startTime);
+  //   for (int i = 0; i < 34; i++) {
+  //     startTime = startTime.add(Duration(minutes: 30));
+  //     _slots.add(startTime);
+  //   }
 
-    //act
-    var result = calculateBestGreenScore(greenScoreList2, _slots);
+  //   //act
+  //   var result = calculateBestGreenScore(greenScoreList2, _slots);
 
-    //assert
-    expect(result, 8);
-  });
+  //   //assert
+  //   expect(result, 9);
+  // });
 
   var greenScoreList3 = [
     GreenScore(hour: 06, minute: 00, greenScore: 8, vacant: false),
