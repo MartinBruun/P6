e07d7aa55234b1db1cf9ab5c1e2d69bcc821aeb9
MartinBruun <mbm93fen@gmail.com>
2022-05-24 00:20:31 +0200

    Turn off linter until some of it can be autodone

--- scripts/win/linting.bat
+++ scripts/win/linting.bat
@@ -4,6 +4,10 @@ cd washee_app
 echo ------------ LINTING FOR APP --------------
 call flutter analyze
 echo ------------ LINTING FOR WEB --------------
+echo AUTOFIX THE EASIEST WITH BLACK
+echo MAKE SURE IT FOLLOWS tox.ini!
 call flake8 washee_web
 echo ------------ LINTING FOR BOX --------------
+echo AUTOFIX THE EASIEST WITH BLACK
+echo MAKE SURE IT FOLLOWS tox.ini!
 call flake8 washee_box
