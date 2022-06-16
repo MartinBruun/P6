1935a0c88dc9776f82f5a16a421ec132c0477edb
MartinBruun <mbm93fen@gmail.com>
2022-06-17 00:28:29 +0200

    Add integration testing template placeholder

---
+++ washee_app/test/templates/integration.dart
@@ -0,0 +1,11 @@
+// NEEDS TEMPLATE!
+
+// Integration tests should be the only tests that are not mocked AT ALL!
+
+// They will describe "user stories" ie. a complete interaction with the system (from a user view), aka. acceptance testing
+
+// They should work and call the desired docker-compose endpoints
+// Such tests are only possible, when a stable testing environment (with known endpoints) can be Setup
+// Should take a bit more planning before jumping in.
+
+// However, the tests should mostly resemble the page_template, given it best represents what users does.
