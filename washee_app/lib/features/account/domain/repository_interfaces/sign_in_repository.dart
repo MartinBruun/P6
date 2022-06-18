89d043b13ad29415ce9281db5205421178bc2a38
MartinBruun <mbm93fen@gmail.com>
2022-06-18 16:57:38 +0200

    Remove unused SignInRepo

--- washee_app/lib/features/account/domain/repository_interfaces/sign_in_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:washee/features/account/data/models/web_account.dart';
-
-abstract class SignInRepository {
-  Future<bool> signIn({required String email, required String password});
-  Future<Account> getAccount(Account account);
-}
