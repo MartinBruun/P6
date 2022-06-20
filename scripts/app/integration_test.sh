02e791968c86e47870d1a131aa30534e3ddbbee7
MartinBruun <mbm93fen@gmail.com>
2022-06-20 16:43:15 +0200

    See if it needs flutter pub get

--- scripts/app/integration_test.sh
+++ scripts/app/integration_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd washee_app
+flutter clean
+flutter pub get
 flutter drive --driver=test_driver/android_driver.dart --target=integration_test/account_test.dart
 #flutter test integration_test
 cd ..
