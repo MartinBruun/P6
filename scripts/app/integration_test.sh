16fc45363c72f464d9e52944d641524a7f5f267e
MartinBruun <mbm93fen@gmail.com>
2022-06-20 14:39:36 +0200

    Try CI again

--- scripts/app/integration_test.sh
+++ scripts/app/integration_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd washee_app
-flutter drive --driver=test_driver/android_driver.dart --target=integration_test/account_test.dart
+#flutter drive --driver=test_driver/android_driver.dart --target=integration_test/account_test.dart
+flutter test integration_test
 cd ..
